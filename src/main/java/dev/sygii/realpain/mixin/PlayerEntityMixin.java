67e6d84a83fa99c28753cf757510fa64ac7254e6
Sygikal <xtin8873@gmail.com>
2025-09-01 18:04:26 -0700

    Handle damage in sethealth and tick only on server

--- src/main/java/dev/sygii/realpain/mixin/PlayerEntityMixin.java
+++ src/main/java/dev/sygii/realpain/mixin/PlayerEntityMixin.java
@@ -2,7 +2,9 @@ package dev.sygii.realpain.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import dev.sygii.realpain.PainAttachments;
+import dev.sygii.realpain.PainMain;
 import dev.sygii.realpain.body.entity.PlayerBody;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -22,6 +24,12 @@ public class PlayerEntityMixin {
 			at = @At("TAIL"))
 	private void init(CallbackInfo info) {
 		PainAttachments.body.set(entity, new PlayerBody(entity));
+		/*if (entity instanceof ServerPlayerEntity) {
+			PainMain.log("server init: " + entity.getMaxHealth());
+		}
+		if (entity instanceof ClientPlayerEntity) {
+			PainMain.log("client init: " + entity.getMaxHealth());
+		}*/
 	}
 
 	@Inject(method = "applyDamage",
@@ -30,7 +38,7 @@ public class PlayerEntityMixin {
 		PainAttachments.body.get(entity).setDistribution(source);
 	}
 
-	@Inject(method = "applyDamage",
+	/*@Inject(method = "applyDamage",
 			at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;addExhaustion(F)V"))
 	private void handleDamage(CallbackInfo info, @Local(argsOnly = true) DamageSource source, @Local(argsOnly = true) float amount) {
 		PainAttachments.body.get(entity).applyDamage(source, amount);
@@ -38,11 +46,13 @@ public class PlayerEntityMixin {
 			PlayerBody body = PainAttachments.body.get(serverPlayerEntity);
 			PainAttachments.body.syncFromServer(serverPlayerEntity, entity, body);
 		}
-	}
+	}*/
 
 	@Inject(method = "tick",
 			at = @At(value = "TAIL"))
 	private void tickBody(CallbackInfo info) {
-		PainAttachments.body.get(entity).tick();
+		if (!entity.getWorld().isClient) {
+			PainAttachments.body.get(entity).tick();
+		}
 	}
 }
